2f2244607995af513d910a51e56a28b25ae9a28d
Aditya Maurya <adityamaurya1718@gmail.com>
2025-10-29 01:47:28 +0530

    /commit 2/backend update

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,7 @@ const subjectRoutes = require('./routes/subject.routes');
 const sectionRoutes = require('./routes/section.routes');
 const timetableRoutes = require('./routes/timetable.routes')
 const dashboardRoutes = require('./routes/dashboard.routes')
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -18,6 +19,7 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true 
 }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/' , (req, res) =>{
     res.send("server started");
@@ -30,9 +32,15 @@ app.use('/api/room', roomRoutes);
 app.use('/api/subject', subjectRoutes);
 app.use('/api/section', sectionRoutes);
 
+
 // --- THIS IS THE CORRECTED LINE ---
 // The base path is now specific, matching the frontend and avoiding conflicts.
 app.use('/api/timetables' , timetableRoutes);
 app.use('/api/dashboard', dashboardRoutes);
+
+
+app.get('*name' , (req ,res)=>{
+    res.sendFile(path.join(__dirname,  '../public/index.html'));
+})
 
 module.exports = app;
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -27,7 +27,13 @@ async function registerAdmin(req, res){
         id: admin._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token" ,token);
+    res.cookie("token" ,token),{
+        httpOnly:true,
+        secure:true,
+        sameSite:'lax',
+        path:'/',
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),  // 1 days
+    };
     res.status(201).json({
         message:"Admin registered successfully",
         admin:{
@@ -60,7 +66,13 @@ async function loginAdmin(req, res){
         id: admin._id,
     },process.env.JWT_SECRET);
 
-    res.cookie("token" , token);
+    res.cookie("token" , token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'lax',
+        path:'/',
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),  // 1 days
+    });
     res.status(201).json({
         message:"user logged in successfully",
         admin:{
