218f34ea19cf6b99cfbdcb6c65dfad1d14d3eec6
Aditya Maurya <adityamaurya1718@gmail.com>
2026-04-08 20:34:45 +0530

    url updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173','https://smart-scheduling-platform.vercel.app'],
+    origin: ['http://localhost:5173','https://smart-scheduling-platform.vercel.app','https://smart-scheduling-platform-qmv4.onrender.com/'],
     credentials: true 
 }));
 app.use(express.static(path.join(__dirname, '../public')));
